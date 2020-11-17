682b6981ae10ef58aa3f9d2ca17803abe6c8a35b
braedinmgregoire <braedinmgregoire@gmail.com>
2020-11-16 22:13:48 -0800

    added more tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -18,5 +18,49 @@ TEST(PracticeTest, is_simple_palindrome)
 {
     Practice obj;
     bool actual = obj.isPalindrome("Not a palindrome");
-    ASSERT_TRUE(actual);
+    ASSERT_FALSE(actual);
+
+		actual = obj.isPalindrome("banaanab");
+		ASSERT_TRUE(actual);
+
+		actual = obj.isPalindrome("baNAnab");
+		ASSERT_TRUE(actual);
+
+		actual = obj.isPalindrome("xbac    ");
+		ASSERT_FALSE(actual);
+
+		actual = obj.isPalindrome("");
+		ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, sortDescending)
+{
+    Practice obj;
+		int first = 2;
+		int second = 3;
+		int third = 4;
+    obj.sortDescending(first, second, third);
+    ASSERT_GE(first, second);
+		ASSERT_GE(second, third);
+
+		first = 4;
+		second = 3;
+		third = 2;
+		obj.sortDescending(first, second, third);
+		ASSERT_GE(first, second);
+		ASSERT_GE(second, third);
+
+		first = 0;
+		second = 0;
+		third = 2;
+		obj.sortDescending(first, second, third);
+		ASSERT_GE(first, second);
+		ASSERT_GE(second, third);
+
+		first = 0;
+		second = 4;
+		third = 0;
+		obj.sortDescending(first, second, third);
+		ASSERT_GE(first, second);
+		ASSERT_GE(second, third);
 }
